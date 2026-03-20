18b0c647d037485b0576657c0f0a4c69aaf0d7e9
TAHIRI Abdelilah <t.abdellilah.t@gmail.com>
2026-03-20 09:56:52 +0000

    feat: Add application logo.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'MedRendezVous')</title>
 
-    <!-- Bootstrap CSS -->
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     @yield('styles')
@@ -14,7 +15,6 @@
 
     @yield('content')
 
-    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
